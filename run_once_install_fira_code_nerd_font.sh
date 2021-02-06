0b6eed13563a7803d7fc13c6116c96a1712aa262
Jan Van Sweevelt <vansweej@gmail.com>
2021-02-06 23:16:13 +0100

    test

--- run_once_install_fira_code_nerd_font.sh
+++ run_once_install_fira_code_nerd_font.sh
@@ -3,6 +3,7 @@ TEMPDIR=$(mktemp --directory --tmpdir 'chezmoi-XXXXXXXXXX-install-fira-code-nerd
 curl --location --output $TEMPDIR/firacode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip $TEMPDIR/firacode.zip -d $TEMPDIR
 mkdir -p ~/.local/share/fonts/firacodenerd
-cp "$TEMPDIR"/*.{otf,ttf} ~/.local/share/fonts/firacodenerd
+cp "$TEMPDIR"/*.otf ~/.local/share/fonts/firacodenerd
+cp "$TEMPDIR"/*.ttf ~/.local/share/fonts/firacodenerd
 #rm -r $TEMPDIR
 fc-cache
