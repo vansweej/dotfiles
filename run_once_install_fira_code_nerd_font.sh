1eb406339b3c562453704b0b78b3bd8c6138b730
Jan Van Sweevelt <vansweej@gmail.com>
2021-02-06 21:18:50 +0100

    add firacode nerd font

---
+++ run_once_install_fira_code_nerd_font.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+TEMPDIR=$(mktemp --directory --tmpdir 'chezmoi-XXXXXXXXXX-install-fira-code-nerd-font')
+curl --location --output $TEMPDIR/firacode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip $TEMPDIR/firacode.zip -d $TEMPDIR
+mkdir -p ~/.local/share/fonts/firacodenerd
+cp "$TEMPDIR/"/*.{otf,ttf} ~/.local/share/fonts/firacodenerd
+rm -r $TEMPDIR
+fc-cache
