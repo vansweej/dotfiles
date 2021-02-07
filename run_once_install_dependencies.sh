fd55de2c69fff87771f60ca24cf40c31be6f7566
Jan Van Sweevelt <vansweej@gmail.com>
2021-02-07 10:26:30 +0100

    added extra packages

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -7,3 +7,4 @@ sudo apt -y upgrade
 echo "installing dependencies"
 sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
+sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
