f833ce988884d070269a0e29a8f101a63533fb22
Jan Van Sweevelt <vansweej@gmail.com>
2021-02-26 12:52:16 +0100

    more dependencies

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -8,4 +8,4 @@ echo "installing dependencies"
 sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
 sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
-sudo apt -y install neofetch conky conky-std gnuplot
+sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive
