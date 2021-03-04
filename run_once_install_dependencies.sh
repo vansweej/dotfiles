4cb93b02113524d17e71751e9174a4835d890c37
Jan Van Sweevelt <vansweej@gmail.com>
2021-03-04 11:36:41 +0100

    more texlive packages

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -8,5 +8,5 @@ echo "installing dependencies"
 sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
 sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
-sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk
-sudo apt -y install gnome-tweaks
+sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
+sudo apt -y install gnome-tweaks 
