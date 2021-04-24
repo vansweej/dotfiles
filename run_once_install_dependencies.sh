43cd8b0962ae3acfe4e7d573098d24da20d64bc3
Jan Van Sweevelt <vansweej@gmail.com>
2021-04-24 17:12:25 +0200

    added neovim

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -9,4 +9,4 @@ sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
 sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
 sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
-sudo apt -y install gnome-tweaks 
+sudo apt -y install gnome-tweaks neovim 
