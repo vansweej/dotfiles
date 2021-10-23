2cc3b626caa58a7fed383357259f5e2da7a66340
Jan Van Sweevelt <vansweej@gmail.com>
2021-10-23 11:16:10 +0200

    added embedded dev stuff

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -11,3 +11,9 @@ sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential lib
 sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
 sudo apt -y install gnome-tweaks neovim libncurses5 nsight-compute guake pylint gcc-11 g++-11
 sudo apt -y install onedrive
+
+# edm
+sudo apt -y install kicad kicad-packages3d
+
+# embedded development
+sudo apt -y install gdb-multiarch minicom openocd
