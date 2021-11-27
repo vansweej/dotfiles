6482b6b7f91dc0b10277402f21fb907653b35969
Jan Van Sweevelt <vansweej@gmail.com>
2021-11-27 16:09:01 +0100

    added ripgrep

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -11,6 +11,7 @@ sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential lib
 sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
 sudo apt -y install gnome-tweaks neovim libncurses5 nsight-compute guake pylint gcc-11 g++-11
 sudo apt -y install onedrive
+sudo apt -y install ripgrep
 
 # edm
 sudo apt -y install kicad kicad-packages3d
