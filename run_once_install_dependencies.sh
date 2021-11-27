ec6abf25ef5567b07c2d77de5722eb5c8560aa2c
Jan Van Sweevelt <vansweej@gmail.com>
2021-11-27 21:17:24 +0100

    installs neovim from ppa

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -9,7 +9,7 @@ sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
 sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
 sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
-sudo apt -y install gnome-tweaks neovim libncurses5 nsight-compute guake pylint gcc-11 g++-11
+sudo apt -y install gnome-tweaks libncurses5 nsight-compute guake pylint gcc-11 g++-11
 sudo apt -y install onedrive
 sudo apt -y install ripgrep
 
@@ -18,3 +18,8 @@ sudo apt -y install kicad kicad-packages3d
 
 # embedded development
 sudo apt -y install gdb-multiarch minicom openocd
+
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt -y update
+sudo apt -y install neovim
+
