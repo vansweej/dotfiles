e229701c2e282c9472082c11a9566e62cf8523b3
Jan Van Sweevelt <vansweej@gmail.com>
2021-11-30 14:27:37 +0100

    added clangd

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -7,7 +7,7 @@ sudo apt -y upgrade
 echo "installing dependencies"
 sudo apt -y install fonts-hack
 sudo apt -y install fonts-firacode
-sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format
+sudo apt -y install wmctrl x11-utils cmake alacritty git-lfs build-essential libsdl2-dev clang clang-format clangd
 sudo apt -y install neofetch conky conky-std gnuplot valgrind texlive texlive-extra-utils latexmk texlive-latex-extra
 sudo apt -y install gnome-tweaks libncurses5 nsight-compute guake pylint gcc-11 g++-11
 sudo apt -y install onedrive
