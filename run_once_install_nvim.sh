81d9fc2ee9ff0c7f29d3c344e80b3e54633f5e8a
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-04 15:42:35 +0100

    finetuning git setup

--- run_once_install_nvim.sh
+++ run_once_install_nvim.sh
@@ -3,3 +3,6 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+nvim +PlugInstall +qall
+
+cd $HOME/.local/share/nvim/plugged/telescope-fzf-native.nvim && make clean && make
