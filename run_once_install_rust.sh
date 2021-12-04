53f7c376ffb48597160fefc8cdeda630b9d67a9b
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-04 10:06:27 +0100

    added extra rust features

--- run_once_install_rust.sh
+++ run_once_install_rust.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+rustup component add rust-src
+
+cargo install cargo-tarpaulin
+cargo install cargo-edit
+cargo install cargo-feature
+
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.cargo/bin/rust-analyzer
+chmod +x ~/.cargo/bin/rust-analyzer
