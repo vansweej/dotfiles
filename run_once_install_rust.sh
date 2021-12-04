68dd0857eca640d487fca89dfdcbe0a69950af9a
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-04 13:22:22 +0100

    added sccache to rust env

--- run_once_install_rust.sh
+++ run_once_install_rust.sh
@@ -7,6 +7,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 ~/.cargo/bin/cargo install cargo-tarpaulin
 ~/.cargo/bin/cargo install cargo-edit
 ~/.cargo/bin/cargo install cargo-feature
+~/.cargo/bin/cargo install sccache
 
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.cargo/bin/rust-analyzer
 chmod +x ~/.cargo/bin/rust-analyzer
