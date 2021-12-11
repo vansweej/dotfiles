9035a5755678aba263cf257ea629cef4e4f68ec7
Jan Van Sweevelt <vansweej@gmail.com>
2021-12-11 22:29:49 +0100

    removed haskell installation, will be handled by nix

--- run_once_install_haskell.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-curl -sSL https://get.haskellstack.org/ | sh
-
-git clone https://github.com/haskell/haskell-language-server --recurse-submodules /tmp/b && cd /tmp/b && stack install.hs hls-8.10.7
-
