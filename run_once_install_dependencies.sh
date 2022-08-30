67988eda0c277aab60f8c9c21699cebcd4cd32ef
Jan Van Sweevelt <vansweej@gmail.com>
2022-08-30 19:50:15 +0200

    changes

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -23,7 +23,7 @@ sudo apt -y install kicad-packages3d
 # embedded development
 sudo apt -y install gdb-multiarch minicom openocd
 
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt -y update
-sudo apt -y install neovim
+#sudo add-apt-repository -y ppa:neovim-ppa/unstable
+#sudo apt -y update
+#sudo apt -y install neovim
 
--- run_once_install_nvim.sh
+++ run_once_install_nvim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim +PlugInstall +qall
+#nvim +PlugInstall +qall
 
-cd $HOME/.local/share/nvim/plugged/telescope-fzf-native.nvim && make clean && make
+#cd $HOME/.local/share/nvim/plugged/telescope-fzf-native.nvim && make clean && make
